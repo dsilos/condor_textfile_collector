48c0c6392ba915fcf5f2f17a8a26c89df08adb71
Eraldo Jr <ejunior@Eraldos-iMac.local>
2020-02-10 12:11:34 -0300

    fix installer

--- node_exporter_install.sh
+++ node_exporter_install.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 echo "Installing node exporter"
-REPO_URL="https://raw.githubusercontent.com/ejr004/"
+REPO_URL="https://raw.githubusercontent.com/ejr004"
 NODE_EXPORTER_VERSION="0.18.1"
 
 # Installing node exporter
@@ -10,14 +10,15 @@ mv /tmp/node_exporter-$NODE_EXPORTER_VERSION.linux-amd64/node_exporter /usr/loca
 
 useradd -M -r -s /bin/false node_exporter
 
+mkdir -p  /var/lib/node_exporter/textfile_collector/
 touch /var/lib/node_exporter/textfile_collector/condor.status
 touch /var/lib/node_exporter/textfile_collector/condor.queue
 chown -R node_exporter:node_exporter /usr/local/bin/node_exporter
 
-wget -O - $REPO_URL/node_exporter.service > /etc/systemd/system/node_exporter.service
+wget -O - $REPO_URL/master/condor_node_exporter.sh > /etc/systemd/system/node_exporter.service
 
 systemctl daemon-reload
 systemctl start node_exporter.service
 systemctl enable node_exporter.service
 
-# wget -O - https://github.com/ejr004/raw/condor_textfile_collector/node_exporter_install.sh | bash
+# wget -O - https://raw.githubusercontent.com/ejr004/condor_textfile_collector/master/node_exporter_install.sh | bash
